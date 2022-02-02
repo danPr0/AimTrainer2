298368d5fab31988317afe8da3a889f52554fc76
danPr0 <94548001+danPr0@users.noreply.github.com>
2022-02-02 18:50:05 +0200

    Update UserRepository.java

--- src/main/java/com/example/repository/UserRepository.java
+++ src/main/java/com/example/repository/UserRepository.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     //List<User> findUserByUsernameEndsWith(String username);
 
-    //Optional<User> findUserByUsername(String username);
+    Optional<User> findUserByUsername(String username);
 
     Boolean existsByUsername(String username);
 }
